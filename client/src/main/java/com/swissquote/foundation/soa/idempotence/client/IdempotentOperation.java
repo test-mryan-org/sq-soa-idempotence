3d93c3bc2c1eafbb62ff368834556cb31e0a4b14
Andrei Niculescu <andrei.niculescu@swissquote.ch>
2015-11-10 17:30:00 +0100

    renamed methods

--- client/src/main/java/com/swissquote/foundation/soa/idempotence/client/IdempotentOperation.java
+++ client/src/main/java/com/swissquote/foundation/soa/idempotence/client/IdempotentOperation.java
@@ -4,30 +4,30 @@ public abstract class IdempotentOperation<T, E extends Throwable> {
 	public T execute() throws E {
 		Long operationId = createNew();
 
-		T result = executeWrappedOperation(operationId);
-		while (!isComplete(result) && stillRetry()) {
+		T result = attemptExecution(operationId);
+		while (!isComplete(result) && canRetry()) {
 			sleep();
-			result = executeWrappedOperation(operationId);
+			result = attemptExecution(operationId);
 			retried();
 		}
 
 		if (isComplete(result)) {
 			return result;
 		}
-		return handleNotCompletedResult();
+		return handleNotCompletedResult(result);
 	}
 
 	public abstract Long createNew();
 
-	public abstract T executeWrappedOperation(Long operationId) throws E;
+	public abstract T attemptExecution(Long operationId) throws E;
 
 	public abstract boolean isComplete(T result);
 
-	public abstract boolean stillRetry();
+	public abstract boolean canRetry();
 
 	public abstract void sleep();
 
-	public abstract T handleNotCompletedResult();
+	public abstract T handleNotCompletedResult(T result);
 
 	public abstract void retried();
 }
